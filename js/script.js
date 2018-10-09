4c76f34facf0b990fd824a42fbe375da6d27dc02
Chris Paisley <lkenn1990@hotmail.com>
2018-10-08 23:22:44 -0700

    added loop to hid elements

--- js/script.js
+++ js/script.js
@@ -4,25 +4,33 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 
 // Add variables that store DOM elements you will need to reference and/or manipulate
+const ul = document.querySelector('.student-list');
+const li = ul.children;
+const listNumb = li.length;
+let pageCounter = 1;
+let pageButton = document.createElement('button');
 
 
+//make function so can be called again
+//if li.length > 10
+  //then hide elements after 10
+//create page numbers with 10 people search
+  //each page number can be clicked and will take to the next 10
+  //if you click a random page it will take you to those 10 people
+//
 
-// Create a function to hide all of the items in the list excpet for the ten you want to show
-// Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
+  if (listNumb > 10){
+    for(let i = 10; i < listNumb; i++){
+      li[i].hidden = true;
+      if (i % 10 === 0) {
+        pageCounter += 1
+        //create button
+        //append button to page div
+        //add pageCounter number to button text
+        console.log(pageCounter);
+        //go to next page
+      }
+    }
 
-
-
-
-// Create and append the pagination links - Creating a function that can do this is a good approach
-
-
-
-
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
-
-
-
-
-
-
+  }
+//every 10 elements show those 10 and hide the previous and the rest.
