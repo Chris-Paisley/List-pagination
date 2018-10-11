037ff86a897ac232b052f89c9c42d96059164b4f
Chris Paisley <lkenn1990@hotmail.com>
2018-10-10 21:31:08 -0700

    now displays 10 students at start and buttons can be clicked but it hides everything and active class isnt removed

--- js/script.js
+++ js/script.js
@@ -3,45 +3,55 @@ const li = ul.children;
 const listNumb = li.length;
 const pageDiv = ul.parentElement;
 const tenStudents = 10;
+const totalPages = Math.ceil(listNumb / tenStudents);
 let pageCounter = 1;
 
-//create button
-  //set button textContent to pageCounter
-  //append button to page div
-function pageButtonFunc (){
-  let pageButton = document.createElement('button');
-  pageButton.textContent = pageCounter;
-  pageDiv.appendChild(pageButton);
-  pageButton.addEventListener('click', () => {
-    //take to page acording to button clicked
 
-  });
-}
-
-//create page numbers with 10 people search
-  //each page number can be clicked and will take to the next 10
-  //if you click a random page it will take you to those 10 people
-
-
-
-
-pageButtonFunc();
-
-
-    for(let i = 10; i < listNumb; i++){
-      if(i <= tenStudents * pageCounter && i > (tenStudents * pageCounter) - tenStudents){
-        li[i].hidden = true;
-      }else {
+const showPage = () => {
+    for(let i = 0; i < listNumb; i++){
+      if(i >= (tenStudents * pageCounter) - tenStudents && i <  tenStudents * pageCounter ){
         li[i].hidden = false;
-      }
-
-      //every 10th student add 1 to the page counter
-      if (i % 10 === 0) {
-        pageCounter ++;
-        pageButtonFunc();
-        //go to next page
+      }else {
+        li[i].hidden = true;
       }
     }
+}
 
 
-//every 10 elements show those 10 and hide the previous and the rest.
+const appendPageLinks = () => {
+  if(ul.classList.contains('pagination')){
+    li[i].classList.remove('pagination');
+  }
+
+  //creates a new div with ul inside with class of pagination
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('pagination');
+  pageDiv.appendChild(newDiv);
+  const newUl = document.createElement('ul');
+  newDiv.appendChild(newUl);
+
+  //for every page needed add a li, a, set the text to the page number and put them in the ul
+  for (let i = 0; i < totalPages; i++) {
+    const newLi = document.createElement('li');
+    const newA = document.createElement('a');
+    newA.textContent = pageCounter;
+    newLi.appendChild(newA);
+    newUl.appendChild(newLi);
+
+    //adds to the page counter for total number of pages so we get the correct amount of page links
+    pageCounter++;
+
+
+    //adds a click event to every page link
+    newLi.addEventListener('click', (e) => {
+      showPage();
+      for (var i = 0; i < totalPages; i++) {
+        newA.classList.remove('active');
+      }
+      e.target.classList.add('active');
+      })
+  }
+
+}
+showPage();
+appendPageLinks();
