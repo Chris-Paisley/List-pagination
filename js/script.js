8b845c227e2d8f1ccd2afd2864356dace5ed6e4c
Chris Paisley <lkenn1990@hotmail.com>
2018-10-11 11:32:34 -0700

    when one first page class is set to active

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const tenStudents = 10;
 const totalPages = Math.ceil(listNumb / tenStudents);
 let pageCounter = 1;
 
-
+//shows only 10 students and will hide others depending on the page clicked
 const showPage = () => {
     for(let i = 0; i < listNumb; i++){
       if(i >= (tenStudents * pageCounter) - tenStudents && i <  tenStudents * pageCounter ){
@@ -15,7 +15,7 @@ const showPage = () => {
         li[i].hidden = true;
       }
     }
-}
+}//end of showPage
 
 
 const appendPageLinks = () => {
@@ -35,18 +35,21 @@ const appendPageLinks = () => {
     const newLi = document.createElement('li');
     const newA = document.createElement('a');
     newA.textContent = pageCounter;
+
+    //if its the first page add active class without it haveing to be clicked
+    if (pageCounter == 1) {
+      newA.classList.add('active');
+    }
     newLi.appendChild(newA);
     newUl.appendChild(newLi);
 
     //adds to the page counter for total number of pages so we get the correct amount of page links
     pageCounter++;
 
-
     //adds a click event to every page link
     newLi.addEventListener('click', (e) => {
       pageCounter = parseInt(e.target.textContent);
       showPage();
-
        //loops over all the links with active class and removes it
          //if it is the target add active class
       const links = document.getElementsByClassName('active');
